7768cf31bdfea5f7673f8227bb62d924fde50468
meph <stnihenr@stud.h-da.de>
2022-12-01 08:55:17 -0500

    t4 types

--- aioquic-main/aioquic-main/examples/http3_client.py
+++ aioquic-main/aioquic-main/examples/http3_client.py
@@ -251,7 +251,7 @@ class HttpClient(QuicConnectionProtocol):
 
 class HttpClientCorruptT4(HttpClient):
     def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+        super().super().__init__(*args, **kwargs)
 
         self.pushes: Dict[int, Deque[H3Event]] = {}
         self._http: Optional[HttpConnection] = None
